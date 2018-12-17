b04f00513f69a136330a05b43fd03abd045d7c23
Arun Kumar <kumarar@c02w83uqhtdd.wwt.com>
2018-12-17 15:50:07 -0600

    course backend

---
+++ src/main/java/com/CourseController.java
@@ -0,0 +1,49 @@
+package com;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.*;
+
+import javax.annotation.PostConstruct;
+import java.util.List;
+
+@Controller
+public class CourseController {
+
+    @Autowired
+    private CourseService courseService;
+
+    @PostMapping("/createcourse")
+    public String createCourse(@ModelAttribute Course course, ModelMap modelMap){
+        Course newCourse = courseService.createCourse(course);
+        modelMap.addAttribute("courseId", newCourse.getCourseId());
+        modelMap.addAttribute("courseName", newCourse.getCourseName());
+        return "courseSuccess";
+    }
+
+    @GetMapping("/lookupcourse")
+    public String findCourse(@RequestParam long courseId, ModelMap modelMap) throws Exception {
+        Course course = courseService.findCourse(courseId);
+        if(course != null){
+            modelMap.addAttribute("courseId", course.getCourseId());
+            modelMap.addAttribute("courseName", course.getCourseName());
+        }else{
+            throw new Exception("Course not found");
+        }
+        return "courseSuccess";
+    }
+
+    @GetMapping("/getstudents")
+    public String getStudentIdsForCourse(@RequestParam long courseId, ModelMap modelMap) throws  Exception{
+        List<Long> studentIds = courseService.getStudentIdsForCourse(courseId);
+        if(studentIds != null){
+            modelMap.addAttribute("studentIdList", studentIds);
+        }else{
+            throw new Exception("Course not found");
+        }
+
+      return "getStudentsSuccess";
+    }
+}
---
+++ src/main/java/com/CourseService.java
@@ -0,0 +1,42 @@
+package com;
+
+import org.springframework.stereotype.Service;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Service
+public class CourseService {
+
+    private static Map<Long, Course> courseMap = new HashMap<>();
+
+    public Course createCourse(Course course){
+
+        Course newCourse;
+        newCourse = new Course(findNextId(), course.getCourseName());
+        courseMap.put(newCourse.getCourseId(), newCourse);
+        return newCourse;
+    }
+
+    public Course findCourse(long courseId){
+        return courseMap.get(courseId);
+    }
+
+    private int findNextId(){
+        return courseMap.size() == 0 ? 1 : courseMap.size()+1;
+    }
+
+    public void linkStudentWithCourse(List<Long> courseIds, long studentId){
+        for(Long courseId: courseIds){
+            Course course = courseMap.get(courseId);
+            course.getStudentIDs().add(studentId);
+            courseMap.put(courseId, course);
+        }
+    }
+
+    public List<Long> getStudentIdsForCourse(long courseId){
+        Course course = courseMap.get(courseId);
+        return course != null ? course.getStudentIDs() : null;
+    }
+}

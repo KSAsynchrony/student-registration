57dd96f994412346a923a6b2959fa72954cfced6
arunkumrs <arunkumrs@github.com>
2019-02-07 10:32:34 -0600

    introduced refresh scope

--- applications/main-server/src/main/java/com/student/registration/main/Application.java
+++ applications/main-server/src/main/java/com/student/registration/main/Application.java
@@ -7,6 +7,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
@@ -18,6 +19,7 @@ import org.springframework.web.client.RestTemplate;
 )
 @EnableEurekaClient
 @EnableCircuitBreaker
+@RefreshScope
 public class Application {
 
     public static void main(String[] args) {
